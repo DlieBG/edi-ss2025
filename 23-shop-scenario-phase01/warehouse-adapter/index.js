4a01124190d1e6d72f6c1265328610c5a9ebc895
Benedikt Schwering <mail@bschwer.ing>
2025-04-15 13:33:57 +0200

    task-01: add items pick messages

--- 23-shop-scenario-phase01/warehouse-adapter/index.js
+++ 23-shop-scenario-phase01/warehouse-adapter/index.js
@@ -1,3 +1,44 @@
 // Imports
 const { Kafka, Partitioners } = require('kafkajs');
 const mqtt = require('mqtt');
+
+const kafka = new Kafka({
+    clientId: 'warehouse-adapter',
+    brokers: ['localhost:9092'],
+});
+const kafka_consumer = kafka.consumer({
+    groupId: 'warehouse-adapter',
+});
+
+const mqtt_client = mqtt.connect('mqtt://localhost:1883');
+
+mqtt_client.on('connect', () => {
+    mqtt_client.subscribe('article/+/stock');
+});
+
+// receive mqtt 'stock' and publish to kafka 'item-stock'
+mqtt_client.on('message', (topic, message) => {
+    const id = /^article\/(.*?)\/stock$/.exec(topic)[1];
+    const stock = parseInt(message);
+
+    console.log(id, stock)
+    // TODO
+});
+
+// receive kafka 'items-pick' and publish to mqtt 'picked'
+const run = async () => {
+    await kafka_consumer.connect();
+    await kafka_consumer.subscribe({ topic: 'items-pick', fromBeginning: true });
+
+    await kafka_consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            const dto = JSON.parse(message.value.toString());
+
+            dto.forEach(item => {
+                mqtt_client.publish(`article/${item.articleId}/picked`, `${item.count}`);
+            });
+        },
+    });
+};
+
+run().catch(console.error);
--- 23-shop-scenario-phase01/warehouse-service/src/main/java/de/thi/informatik/edi/shop/warehouse/connectors/KafkaProducer.java
+++ 23-shop-scenario-phase01/warehouse-service/src/main/java/de/thi/informatik/edi/shop/warehouse/connectors/KafkaProducer.java
@@ -1,12 +1,16 @@
 package de.thi.informatik.edi.shop.warehouse.connectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.thi.informatik.edi.shop.warehouse.connectors.dto.PickItemDto;
 import de.thi.informatik.edi.shop.warehouse.connectors.dto.UpdateShippingDto;
 import de.thi.informatik.edi.shop.warehouse.model.Shipping;
+import de.thi.informatik.edi.shop.warehouse.model.ShippingItem;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class KafkaProducer {
@@ -23,5 +27,20 @@ public class KafkaProducer {
         String dtoJson = objectMapper.writeValueAsString(dto);
 
         this.template.send("shipping-update", dtoJson);
+    }
+
+    @SneakyThrows
+    public void pickItems(List<ShippingItem> items) {
+        List<PickItemDto> dto = items.stream().map(
+                item -> new PickItemDto(
+                        item.getArticle(),
+                        item.getCount()
+                )
+        ).toList();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String dtoJson = objectMapper.writeValueAsString(dto);
+
+        this.template.send("items-pick", dtoJson);
     }
 }
